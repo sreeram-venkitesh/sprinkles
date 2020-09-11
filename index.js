17f172139d4ab3ade33f29333ea3e6324c0b5ccd
fillerInk <thatonephysicsnerd@gmail.com>
2020-09-11 13:12:20 +0530

    Registering userand redirect complete

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ const path = require("path");
 const cors = require("cors");
 const { Client } = require('pg')
 const { pool } = require('./dbConfig')
+const bcrypt = require('bcrypt')
 
+const session = require('express-session')
+const flash = require('express-flash')
 
 const app = express()
 
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
+app.use(session({
+    secret:'secret',
+    resave:false,
+    saveUninitialized: false,
+}))
+app.use(flash())
 
 app.use(express.static(__dirname + '/views'))
 app.set('view engine', 'ejs')
@@ -90,7 +98,7 @@ client
 // })
 
 
-app.get('/',(req,res)=>{
+app.get('/login',(req,res)=>{
     res.render('html/index')
 })
 
@@ -102,7 +110,7 @@ app.get('/dashboard',(req,res)=>{
     res.render('html/dashboard',{ user: "Sreeram"})
 })
 
-app.post('/signup',(req,res)=>{
+app.post('/signup', async (req,res)=>{
     let { name, email, password, password2 } = req.body;
 
     console.log({
@@ -128,7 +136,33 @@ app.post('/signup',(req,res)=>{
         res.render('html/signup',{ errors })
     }else{
         // Form validation passed
-        
+        let hashedPassword = await bcrypt.hash(password, 10)
+        console.log(hashedPassword)
+
+        pool.query(
+            `SELECT * FROM users
+            WHERE email = $1`, [email], (err,result) => {
+                if(err) throw err
+                console.log(result.rows)
+
+                if(result.rows.length > 0){
+                    errors.push({ message: "Email already in use"})
+                    res.render('html/signup', {errors})
+                }else{
+                    pool.query(
+                        `INSERT INTO users (name,email,password)
+                        VALUES($1, $2, $3)
+                        RETURNING id, password`, [name, email, hashedPassword], (err,result)=>{
+                            if(err) throw error
+                            console.log(result)
+                            // req.flash('success-message', 'You are now registered, login to your account')
+                            const expressFlash = "You are now registered, login to your account"
+                            res.render('html/index',{ expressFlash })
+                        }
+                    )
+                }
+            }
+        )
     }
 })
 
