a212b3fc9b25828a7a67696292f3226b8c6c8c4d
fillerInk <thatonephysicsnerd@gmail.com>
2020-09-11 12:28:11 +0530

    Form validation for signup

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const path = require("path");
 const cors = require("cors");
 const { Client } = require('pg')
+const { pool } = require('./dbConfig')
+
 
 const app = express()
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
 app.use(express.static(__dirname + '/views'))
 app.set('view engine', 'ejs')
 
@@ -23,10 +27,11 @@ client.connect();
 
 const query = `
 CREATE TABLE users (
-    email varchar,
-    firstName varchar,
-    lastName varchar,
-    age int
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    UNIQUE(email)
 );
 `;
 
@@ -44,17 +49,17 @@ FROM users
 const q = `DROP TABLE users`
 
 //creating table
-// client
-//     .query(query)
-//     .then(res => {
-//         console.log('Table is successfully created');
-//     })
-//     .catch(err => {
-//         console.error(err);
-//     })
-//     .finally(() => {
-//         client.end();
-//     });
+client
+    .query(query)
+    .then(res => {
+        console.log('Table is successfully created');
+    })
+    .catch(err => {
+        console.error(err);
+    })
+    .finally(() => {
+        client.end();
+    });
 
 //inserting rows
 // client.query(query3, (err, res) => {
@@ -67,16 +72,16 @@ const q = `DROP TABLE users`
 // });
 
 // displaying all rows
-client.query(query4, (err, res) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    for (let row of res.rows) {
-        console.log(row);
-    }
-    client.end();
-});
+// client.query(query4, (err, res) => {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//     for (let row of res.rows) {
+//         console.log(row);
+//     }
+//     client.end();
+// });
 
 //dropping a table
 // client.query(q,(err,res)=>{
@@ -87,6 +92,44 @@ client.query(query4, (err, res) => {
 
 app.get('/',(req,res)=>{
     res.render('html/index')
+})
+
+app.get('/signup',(req,res)=>{
+    res.render('html/signup')
+})
+
+app.get('/dashboard',(req,res)=>{
+    res.render('html/dashboard',{ user: "Sreeram"})
+})
+
+app.post('/signup',(req,res)=>{
+    let { name, email, password, password2 } = req.body;
+
+    console.log({
+        name,
+        email,
+        password,
+        password2
+    })
+
+    let errors = []
+
+    if(!name || !email || !password || !password2){
+        errors.push({message: "Please enter all fields"})
+    }
+    if(password.length < 6 ){
+        errors.push({message: "Password should atleast be 6 characters long"})
+    }
+    if(password != password2){
+        errors.push({message: "Passwords do not match"})
+    }
+
+    if(errors.length > 0){
+        res.render('html/signup',{ errors })
+    }else{
+        // Form validation passed
+        
+    }
 })
 
 const PORT = process.env.PORT || 3000
